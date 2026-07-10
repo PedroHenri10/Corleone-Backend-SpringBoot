b03e486573062b7d767831d7daf0bbb3030974f6
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-10 13:38:41 -0300

    feat: adicionando entidade forma pagamento com jpa

--- src/main/java/com/corleone/estoque/entity/Estoque.java
+++ src/main/java/com/corleone/estoque/entity/Estoque.java
@@ -1,4 +1,51 @@
 package com.corleone.estoque.entity;
 
+import com.corleone.funcionario.entity.Funcionario;
+import com.corleone.produto.entity.Produto;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "estoque")
 public class Estoque {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "est_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "pro_id")
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "fun_id")
+    private Funcionario funcionario;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "est_tipo")
+    private TipoMovimentacao tipo;
+
+    @Column(name = "est_quantidade", precision = 10, scale = 3)
+    private BigDecimal quantidade;
+
+    @Column(name = "est_valor_unitario", precision = 10, scale = 2)
+    private BigDecimal valorUnitario;
+
+    @Column(name = "est_documento")
+    private String documento;
+
+    @Column(name = "est_observacao")
+    private String observacao;
+
+    @Column(name = "est_dt_movimentacao")
+    private LocalDateTime dataMovimentacao;
 }
--- src/main/java/com/corleone/pagamento/entity/Pagamento.java
+++ src/main/java/com/corleone/pagamento/entity/Pagamento.java
@@ -1,4 +1,27 @@
 package com.corleone.pagamento.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "pagamento")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "fpg_id")
+    private Long id;
+
+    @Column(name = "fpg_nome", length = 60)
+    private String nome;
+
+    @Column(name = "fpg_requer_troco")
+    private Boolean requerTroco;
+
+    @Column(name = "fpg_ativo")
+    private Boolean ativo;
 }
