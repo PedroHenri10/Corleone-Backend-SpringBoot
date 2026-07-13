8ce9f228aa4b1ba1e697f315022838314eb7fb8a
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-13 15:04:37 -0300

    feat: configuracao swagger e documentacao OpenAPI

--- src/main/java/com/corleone/config/OpenApiConfig.java
+++ src/main/java/com/corleone/config/OpenApiConfig.java
@@ -1,4 +1,27 @@
 package com.corleone.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Corleone API")
+                                .version("1.0.0")
+                                .description("API do sistema de gestão da Pizzaria Corleone")
+                                .contact(
+                                        new Contact()
+                                                .name("Pedro Henrique")
+                                )
+                );
+    }
 }
