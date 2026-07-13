38f6d6d5e34eb80ef910bf27f6ec18f77646aae6
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-13 14:50:44 -0300

    feat: adicionado entidade sabor via jpa

---
+++ src/main/java/com/corleone/produto/entity/ProdutoSabor.java
@@ -0,0 +1,31 @@
+package com.corleone.produto.entity;
+
+import com.corleone.sabor.entity.Sabor;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(
+        name = "produto_sabor",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"pro_id", "sab_id"})
+)
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoSabor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "prs_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pro_id", nullable = false)
+    private Produto produto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sab_id", nullable = false)
+    private Sabor sabor;
+}
--- src/main/java/com/corleone/sabor/entity/Sabor.java
+++ src/main/java/com/corleone/sabor/entity/Sabor.java
@@ -1,4 +1,28 @@
 package com.corleone.sabor.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "sabor")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Sabor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sab_id")
+    private Long id;
+
+    @Column(name = "sab_nome", length = 100)
+    private String nome;
+
+    @Column(name = "sab_descricao", length = 255)
+    private String descricao;
+
+    @Column(name = "sab_ativo")
+    private Boolean ativo;
 }
