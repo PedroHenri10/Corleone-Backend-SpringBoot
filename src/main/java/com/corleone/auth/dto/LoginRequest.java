8bbbb2e55ae613e7914f1a59e58e85e7ba110441
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-14 15:55:15 -0300

    feat(auth): login response e request implementados

--- src/main/java/com/corleone/auth/dto/LoginRequest.java
+++ src/main/java/com/corleone/auth/dto/LoginRequest.java
@@ -1,4 +1,13 @@
 package com.corleone.auth.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class LoginRequest {
+
+    private String username;
+
+    private String password;
 }
--- src/main/java/com/corleone/auth/dto/LoginResponse.java
+++ src/main/java/com/corleone/auth/dto/LoginResponse.java
@@ -1,4 +1,13 @@
 package com.corleone.auth.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class LoginResponse {
+
+    private String token;
 }
