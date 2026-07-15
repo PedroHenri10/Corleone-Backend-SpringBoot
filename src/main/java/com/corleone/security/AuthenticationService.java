0c10dd61defcfb170247d1102d42006165e85dc4
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-15 10:11:33 -0300

    feat(security): criando login fake inicial

--- src/main/java/com/corleone/security/AuthenticationService.java
+++ src/main/java/com/corleone/security/AuthenticationService.java
@@ -1,4 +1,21 @@
 package com.corleone.security;
 
+import com.corleone.auth.dto.LoginRequest;
+import com.corleone.auth.dto.LoginResponse;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthenticationService {
+
+    public LoginResponse login(LoginRequest request) {
+
+        if (!"admin".equals(request.getUsername()) || !"123456".equals(request.getPassword())
+        ) {
+            throw new RuntimeException("Credenciais inválidas");
+        }
+
+        return LoginResponse.builder()
+                .token("LOGIN_FAKE_SPRINT_0")
+                .build();
+    }
 }
--- src/main/java/com/corleone/security/JwtService.java
+++ src/main/java/com/corleone/security/JwtService.java
@@ -1,4 +1,20 @@
 package com.corleone.security;
+/*
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import io.jsonwebtoken.security.Keys;
 
+import java.security.Key;
+
+@Service
 public class JwtService {
+    private final Key chave;
+    private final long jwtExpiration;
+
+    public JwtService(@Value("${jwt.secret}") String jwtSecret,
+                      @Value("${jwt.expiration}") long jwtExpiration){
+        this.chave = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+        this.jwtExpiration = jwtExpiration;
+    }
 }
+*/
