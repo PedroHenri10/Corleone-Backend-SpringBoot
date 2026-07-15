1c1688f63c84268021d563d56cc1ff1562d80fd8
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-15 12:23:30 -0300

    feat(security): implementa user details service

--- src/main/java/com/corleone/security/UserDetailsService.java
+++
@@ -1,4 +0,0 @@
-package com.corleone.security;
-
-public class UserDetailsService {
-}
---
+++ src/main/java/com/corleone/security/UserDetailsServiceImpl.java
@@ -0,0 +1,44 @@
+package com.corleone.security;
+
+import com.corleone.usuario.entity.RolePermissao;
+import com.corleone.usuario.entity.Usuario;
+import com.corleone.usuario.entity.UsuarioRole;
+import com.corleone.usuario.repository.UsuarioRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByLoginAndAtivoTrue(username).
+                orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+
+        for (UsuarioRole usuarioRole : usuario.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(usuarioRole.getRole().getNome()));
+
+            for (RolePermissao permissao: usuarioRole.getRole().getPermissoes())
+            {
+                authorities.add(new SimpleGrantedAuthority(permissao.getPermissao().getNome())
+                );
+            }
+        }
+
+        return
+                User.builder().username(usuario.getLogin()).password(usuario.getSenha()).authorities(authorities)
+                        .disabled(!usuario.getAtivo()).build();
+    }
+}
