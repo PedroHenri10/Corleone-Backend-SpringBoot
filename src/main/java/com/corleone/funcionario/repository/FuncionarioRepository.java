fa452c4b4cdae1cd01fe8a05a4f3222d009f4ce3
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-16 16:02:40 -0300

    feat(funcionario): cria repositories do modulo funcionario

--- src/main/java/com/corleone/funcionario/repository/FuncionarioRepository.java
+++ src/main/java/com/corleone/funcionario/repository/FuncionarioRepository.java
@@ -1,4 +1,21 @@
 package com.corleone.funcionario.repository;
 
-public interface FuncionarioRepository {
+import com.corleone.funcionario.entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface FuncionarioRepository extends
+        JpaRepository<Funcionario, Integer>,
+        JpaSpecificationExecutor<Funcionario> {
+
+    Optional<Funcionario> findByFunCpf(String cpf);
+
+    Optional<Funcionario> findByFunEmail(String email);
+
+    boolean existsByFunCpf(String cpf);
+
+    boolean existsByFunEmail(String email);
+
 }
