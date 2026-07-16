5fda9d6372ee92af3a7503a64be40d6ef41e0f08
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-16 16:07:19 -0300

    feat(funcionario): cria specification do modulo funcionario

---
+++ src/main/java/com/corleone/funcionario/specification/FuncionarioSpecification.java
@@ -0,0 +1,83 @@
+package com.corleone.funcionario.specification;
+
+import com.corleone.funcionario.entity.Funcionario;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+
+public class FuncionarioSpecification {
+
+    public static Specification<Funcionario> nome(String nome) {
+
+        return (root, query, cb) ->
+                nome == null
+                        ? null
+                        : cb.like(
+                        cb.lower(root.get("funNome")),
+                        "%" + nome.toLowerCase() + "%");
+    }
+
+    public static Specification<Funcionario> cpf(String cpf) {
+
+        return (root, query, cb) ->
+                cpf == null
+                        ? null
+                        : cb.equal(root.get("funCpf"), cpf);
+    }
+
+    public static Specification<Funcionario> cargo(Integer cargoId) {
+
+        return (root, query, cb) ->
+                cargoId == null
+                        ? null
+                        : cb.equal(root.get("cargo").get("carId"), cargoId);
+    }
+
+    public static Specification<Funcionario> ativo(Boolean ativo) {
+
+        return (root, query, cb) ->
+                ativo == null
+                        ? null
+                        : cb.equal(root.get("funAtivo"), ativo);
+    }
+
+    public static Specification<Funcionario> cidade(String cidade) {
+
+        return (root, query, cb) ->
+                cidade == null
+                        ? null
+                        : cb.like(
+                        cb.lower(root.get("endereco").get("endCidade")),
+                        "%" + cidade.toLowerCase() + "%");
+    }
+
+    public static Specification<Funcionario> dataAdmissao(
+            LocalDate inicio,
+            LocalDate fim) {
+
+        return (root, query, cb) -> {
+
+            if (inicio == null && fim == null) {
+                return null;
+            }
+
+            if (inicio != null && fim != null) {
+                return cb.between(
+                        root.get("funDataAdmissao"),
+                        inicio,
+                        fim);
+            }
+
+            if (inicio != null) {
+                return cb.greaterThanOrEqualTo(
+                        root.get("funDataAdmissao"),
+                        inicio);
+            }
+
+            return cb.lessThanOrEqualTo(
+                    root.get("funDataAdmissao"),
+                    fim);
+        };
+    }
+
+}
