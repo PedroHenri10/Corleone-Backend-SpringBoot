122c36f93631d0f8aa50c1899b97ff5bec1fc6fc
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-20 08:47:16 -0300

    feat(cargo): add cargo repository

--- src/main/java/com/corleone/cargo/repository/CargoRepository.java
+++ src/main/java/com/corleone/cargo/repository/CargoRepository.java
@@ -1,4 +1,14 @@
 package com.corleone.cargo.repository;
 
-public interface CargoRepository {
+import com.corleone.cargo.entity.Cargo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CargoRepository extends JpaRepository<Cargo, Integer> {
+
+    Optional<Cargo> findByCarNome(String nome);
+
+    boolean existsByCarNome(String nome);
+
 }
