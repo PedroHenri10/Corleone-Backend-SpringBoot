c72d7d3515708fec5694274d38d4e3ed9639f01c
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-27 12:10:08 -0300

    feat(ingrediente): interfaces repository e mapper

--- src/main/java/com/corleone/ingrediente/mapper/IngredienteMapper.java
+++ src/main/java/com/corleone/ingrediente/mapper/IngredienteMapper.java
@@ -1,4 +1,49 @@
 package com.corleone.ingrediente.mapper;
 
-public class IngredienteMapper {
+import com.corleone.ingrediente.dto.*;
+import com.corleone.ingrediente.entity.Ingrediente;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface IngredienteMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "ativo", ignore = true)
+    @Mapping(target = "dataCriacao", ignore = true)
+    @Mapping(target = "dataAtualizacao", ignore = true)
+    @Mapping(target = "nome", source = "request.nome")
+    @Mapping(target = "descricao", source = "request.descricao")
+    @Mapping(target = "unidade", source = "request.unidade")
+    @Mapping(target = "custoUnitario", source = "request.custoUnitario")
+    @Mapping(target = "estoqueMinimo", source = "request.estoqueMinimo")
+    Ingrediente toEntity(IngredienteRequest request);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "nome", source = "nome")
+    @Mapping(target = "descricao", source = "descricao")
+    @Mapping(target = "unidade", source = "unidade")
+    @Mapping(target = "custoUnitario", source = "custoUnitario")
+    @Mapping(target = "estoqueMinimo", source = "estoqueMinimo")
+    @Mapping(target = "ativo", source = "ativo")
+    @Mapping(target = "dataCriacao", source = "dataCriacao")
+    @Mapping(target = "dataAtualizacao", source = "dataAtualizacao")
+    IngredienteResponse toResponse(Ingrediente ingrediente);
+
+    @Mapping(target = "id", source = "id")
+    @Mapping(target = "nome", source = "nome")
+    @Mapping(target = "unidade", source = "unidade")
+    @Mapping(target = "ativo", source = "ativo")
+    IngredienteResumoResponse toResumoResponse(Ingrediente ingrediente);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "ativo", ignore = true)
+    @Mapping(target = "dataCriacao", ignore = true)
+    @Mapping(target = "dataAtualizacao", ignore = true)
+    @Mapping(target = "nome", source = "request.nome")
+    @Mapping(target = "descricao", source = "request.descricao")
+    @Mapping(target = "unidade", source = "request.unidade")
+    @Mapping(target = "custoUnitario", source = "request.custoUnitario")
+    @Mapping(target = "estoqueMinimo", source = "request.estoqueMinimo")
+    void updateEntity(@MappingTarget Ingrediente ingrediente, IngredienteRequest request);
+
 }
--- src/main/java/com/corleone/ingrediente/repository/IngredienteRepository.java
+++ src/main/java/com/corleone/ingrediente/repository/IngredienteRepository.java
@@ -2,7 +2,14 @@ package com.corleone.ingrediente.repository;
 
 import com.corleone.ingrediente.entity.Ingrediente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface IngredienteRepository extends JpaRepository<Ingrediente, Integer> {
+import java.util.Optional;
 
+public interface IngredienteRepository extends JpaRepository<Ingrediente, Integer>, JpaSpecificationExecutor<Ingrediente> {
+    Optional<Ingrediente> findByNomeIgnoreCase(String nome);
+
+    boolean existsByNomeIgnoreCase(String nome);
+
+    boolean existsByNomeIgnoreCaseAndIdNot(String nome, Integer id);
 }
