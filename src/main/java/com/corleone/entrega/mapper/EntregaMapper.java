62c4ce2ac565bddc96b074c31f8dae840297d2d7
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-29 10:53:09 -0300

    feat(entrega): interfaces repository e mapper

--- src/main/java/com/corleone/entrega/mapper/EntregaMapper.java
+++ src/main/java/com/corleone/entrega/mapper/EntregaMapper.java
@@ -1,4 +1,37 @@
 package com.corleone.entrega.mapper;
 
-public class EntregaMapper {
+import com.corleone.entrega.dto.AtualizarStatusEntregaRequest;
+import com.corleone.entrega.dto.EntregaRequest;
+import com.corleone.entrega.dto.EntregaResponse;
+import com.corleone.entrega.dto.EntregaResumoResponse;
+import com.corleone.entrega.entity.Entrega;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface EntregaMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "pedido", ignore = true)
+    @Mapping(target = "entregador", ignore = true)
+    @Mapping(target = "endereco", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "dataSaida", ignore = true)
+    @Mapping(target = "dataEntrega", ignore = true)
+    Entrega toEntity(EntregaRequest request);
+
+    @Mapping(target = "pedidoId", source = "pedido.id")
+    @Mapping(target = "entregadorId", source = "entregador.id")
+    @Mapping(target = "entregador", source = "entregador.nome")
+    @Mapping(target = "enderecoId", source = "endereco.id")
+    EntregaResponse toResponse(Entrega entrega);
+
+    @Mapping(target = "pedidoId", source = "pedido.id")
+    @Mapping(target = "entregador", source = "entregador.nome")
+    EntregaResumoResponse toResumoResponse(Entrega entrega);
+
+    void updateEntity(AtualizarStatusEntregaRequest request, @MappingTarget Entrega entrega);
+
 }
--- src/main/java/com/corleone/entrega/repository/EntregaRepository.java
+++ src/main/java/com/corleone/entrega/repository/EntregaRepository.java
@@ -1,4 +1,17 @@
 package com.corleone.entrega.repository;
 
-public interface EntregaRepository {
+import com.corleone.entrega.entity.Entrega;
+import com.corleone.shared.enums.StatusEntrega;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface EntregaRepository extends JpaRepository<Entrega, Integer>, JpaSpecificationExecutor<Entrega> {
+
+    Optional<Entrega> findByPedidoId(Integer pedidoId);
+
+    List<Entrega> findAllByStatus(StatusEntrega status);
+
 }
