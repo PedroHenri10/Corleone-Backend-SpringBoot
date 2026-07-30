3774211cc780c908c10b5d823a61b5f56c11d348
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-30 11:48:15 -0300

    feat(mesa): interface do mapper

--- src/main/java/com/corleone/mesa/mapper/MesaMapper.java
+++ src/main/java/com/corleone/mesa/mapper/MesaMapper.java
@@ -1,4 +1,35 @@
 package com.corleone.mesa.mapper;
 
-public class MesaMapper {
+import com.corleone.mesa.dto.MesaRequest;
+import com.corleone.mesa.dto.MesaResponse;
+import com.corleone.mesa.dto.MesaResumoResponse;
+import com.corleone.mesa.entity.Mesa;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface MesaMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dataCriacao", ignore = true)
+    @Mapping(target = "ativo", constant = "true")
+    @Mapping(target = "status", constant = "LIVRE")
+    /*Como o nome dos campos é igual (numero e capacidade), o MapStruct faz esse mapeamento automaticamente.
+     Então essas duas linhas são desnecessárias.*/
+    @Mapping(target = "numero", source = "request.numero")
+    @Mapping(target = "capacidade", source = "request.capacidade")
+    Mesa toEntity(MesaRequest request);
+
+
+    MesaResponse toResponse(Mesa mesa);
+
+    MesaResumoResponse toResumoResponse(Mesa mesa);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "dataCriacao", ignore = true)
+    @Mapping(target = "ativo", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    void updateEntity(@MappingTarget Mesa mesa, MesaRequest request);
 }
