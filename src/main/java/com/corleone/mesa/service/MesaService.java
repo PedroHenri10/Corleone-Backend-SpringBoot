ac3e85480079a3f97de6cfacae106f94376d97ab
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-31 15:54:36 -0300

    fix(mesa): criando mesa

--- src/main/java/com/corleone/mesa/service/MesaService.java
+++ src/main/java/com/corleone/mesa/service/MesaService.java
@@ -1,4 +1,35 @@
 package com.corleone.mesa.service;
 
+import com.corleone.mesa.dto.MesaRequest;
+import com.corleone.mesa.dto.MesaResponse;
+import com.corleone.mesa.entity.Mesa;
+import com.corleone.mesa.mapper.MesaMapper;
+import com.corleone.mesa.repository.MesaRepository;
+import com.corleone.mesa.validator.MesaValidator;
+import com.corleone.shared.util.DateUtils;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class MesaService {
+    private final MesaRepository repository;
+    private final MesaMapper mapper;
+    private final MesaValidator validator;
+
+    public MesaResponse criar(MesaRequest request){
+        validator.validarNumeroDuplicado(request.getNumero());
+
+        Mesa mesa = mapper.toEntity(request);
+
+        mesa.setDataCriacao(LocalDateTime.now(DateUtils.BR_ZONE));
+
+        mesa = repository.save(mesa);
+
+        return mapper.toResponse(mesa);
+    }
 }
