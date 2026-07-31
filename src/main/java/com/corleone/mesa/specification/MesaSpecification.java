4f8c6761e5dbccf7075f2d864684cc63e2273e2d
Pedro Henrique <dinhonoliver@gmail.com>
2026-07-31 15:34:27 -0300

    fix(mesa): specification ativo

--- src/main/java/com/corleone/mesa/specification/MesaSpecification.java
+++ src/main/java/com/corleone/mesa/specification/MesaSpecification.java
@@ -11,5 +11,15 @@ public class MesaSpecification {
                 : cb.equal(root.get("numero"), numero);
     }
 
-    
+    public static Specification<Mesa> ativo(Boolean ativo) {
+
+        return (root, query, cb) ->
+                ativo == null
+                        ? null
+                        : cb.equal(root.get("ativo"), ativo);
+    }
+
+    public static Specification<Mesa> somenteAtivos(){
+        return ((root, query, cb) -> cb.isTrue(root.get("ativo")));
+    }
 }
