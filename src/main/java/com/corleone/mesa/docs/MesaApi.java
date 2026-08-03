7f7617e10310d624ed33bc22022f5076c556de20
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-03 10:13:49 -0300

    feat(mesa): crud principal da mesa

---
+++ src/main/java/com/corleone/mesa/docs/MesaApi.java
@@ -0,0 +1,67 @@
+package com.corleone.mesa.docs;
+
+import com.corleone.mesa.dto.MesaFilter;
+import com.corleone.mesa.dto.MesaRequest;
+import com.corleone.mesa.dto.MesaResponse;
+import com.corleone.mesa.dto.MesaResumoResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "Mesa", description = "Gerenciamento de Mesas do sistema.")
+public interface MesaApi {
+    @Operation(summary = "Cadastrar mesa", description = "Realiza o cadastro de uma nova mesa..",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Mesa cadastrado com sucesso.", content = @Content(
+                            schema = @Schema(implementation = MesaResponse.class))),
+                    @ApiResponse(responseCode = "409", description = "Mesa já cadastrados.")
+            }
+    )
+    ResponseEntity<MesaResponse> criar(MesaRequest request);
+
+    @Operation(
+            summary = "Buscar mesa por ID",
+            description = "Retorna os dados completos de uma mesa.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "mesa encontrada.", content = @Content(
+                            schema = @Schema(implementation = MesaResponse.class))),
+                    @ApiResponse(responseCode = "404", description = "Mesa não encontrada.")
+            }
+    )
+    ResponseEntity<MesaResponse> buscarPorId(@Parameter(description = "ID da mesa.", example = "1") Integer id);
+
+    @Operation(summary = "Listar mesas", description = "Lista mesas utilizando paginação e filtros.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Lista retornada com sucesso.", content = @Content(
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = MesaResumoResponse.class))))
+            }
+    )
+    ResponseEntity<Page<MesaResumoResponse>> listar(MesaFilter filtro, Pageable pageable);
+
+    @Operation(summary = "Atualizar mesa", description = "Atualiza os dados de uma mesa.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "mesa atualizada."),
+                    @ApiResponse(responseCode = "404", description = "mesa não encontrada."),
+                    @ApiResponse(responseCode = "409", description = "Já existe uma mesa com esse número."),
+                    @ApiResponse(responseCode = "422", description = "A operação não pode ser realizada porque a mesa está inativa.")
+            }
+    )
+    ResponseEntity<MesaResponse> atualizar(@Parameter(description = "ID do funcionário.", example = "1") Integer id, MesaRequest request);
+
+    @Operation(summary = "Desativar mesa", description = "Realiza a exclusão lógica da mesa.",
+            responses = {
+                    @ApiResponse(responseCode = "204", description = "Mesa desativada."),
+                    @ApiResponse(responseCode = "404", description = "Mesa não encontrada."),
+                    @ApiResponse(responseCode = "422", description = "Mesa já inativq.", content = @Content(schema = @Schema(hidden = true)))
+            }
+    )
+    ResponseEntity<Void> desativar(@Parameter(description = "ID da mesa.", example = "1") Integer id);
+}
