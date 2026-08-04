a1f1fd636cfd4b26a541268e27e4900a09c8e51d
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-04 13:54:29 -0300

    feat(avaliacao): cliente specification

--- src/main/java/com/corleone/avaliacao/specification/AvaliacaoSpecification.java
+++ src/main/java/com/corleone/avaliacao/specification/AvaliacaoSpecification.java
@@ -1,4 +1,19 @@
 package com.corleone.avaliacao.specification;
 
+import com.corleone.avaliacao.dto.AvaliacaoFilter;
+import com.corleone.avaliacao.entity.Avaliacao;
+import org.springframework.data.jpa.domain.Specification;
+
 public class AvaliacaoSpecification {
+
+    public static Specification<Avaliacao> cliente(Integer clienteId) {
+
+        return (root, query, cb) ->
+                clienteId == null
+                        ? null
+                        : cb.equal(root.get("cliente").get("id"), clienteId);
+    }
+
+   
+
 }
