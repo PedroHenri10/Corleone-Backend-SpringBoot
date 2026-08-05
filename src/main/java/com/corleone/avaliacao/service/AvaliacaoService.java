00d9e87cbe4f2848c47a899b5f22db64295b760b
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-05 15:58:39 -0300

    feat(avaliacao): logica de criar avaliacao

--- src/main/java/com/corleone/avaliacao/service/AvaliacaoService.java
+++ src/main/java/com/corleone/avaliacao/service/AvaliacaoService.java
@@ -1,4 +1,50 @@
 package com.corleone.avaliacao.service;
 
+import com.corleone.avaliacao.dto.AvaliacaoFilter;
+import com.corleone.avaliacao.dto.AvaliacaoRequest;
+import com.corleone.avaliacao.dto.AvaliacaoResponse;
+import com.corleone.avaliacao.dto.AvaliacaoResumoResponse;
+import com.corleone.avaliacao.entity.Avaliacao;
+import com.corleone.avaliacao.mapper.AvaliacaoMapper;
+import com.corleone.avaliacao.repository.AvaliacaoRepository;
+import com.corleone.avaliacao.specification.AvaliacaoSpecification;
+import com.corleone.avaliacao.validator.AvaliacaoValidator;
+import com.corleone.cliente.entity.Cliente;
+import com.corleone.pedido.entity.Pedido;
+import com.corleone.shared.util.DateUtils;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class AvaliacaoService {
+
+    private final AvaliacaoRepository repository;
+    private final AvaliacaoMapper mapper;
+    private final AvaliacaoValidator validator;
+
+    public AvaliacaoResponse criar(AvaliacaoRequest request) {
+
+        validator.validarNota(request.getNota());
+
+        validator.validarPedidoJaAvaliado(request.getPedidoId());
+
+        Pedido pedido = validator.validarPedido(request.getPedidoId());
+
+        Cliente cliente = validator.validarCliente(request.getClienteId());
+
+        Avaliacao avaliacao = mapper.toEntity(request, pedido, cliente);
+
+        avaliacao.setDataCriacao(LocalDateTime.now(DateUtils.BR_ZONE));
+
+        avaliacao = repository.save(avaliacao);
+
+        return mapper.toResponse(avaliacao);
+    }
+
 }
