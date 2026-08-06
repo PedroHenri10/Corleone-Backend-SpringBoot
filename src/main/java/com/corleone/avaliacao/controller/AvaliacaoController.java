f21d49a897d6c0bd94b0fab1ff325e67b52e794e
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-06 16:30:43 -0300

    feat(avaliacao): removendo importacoes desnecessarias

--- src/main/java/com/corleone/avaliacao/controller/AvaliacaoController.java
+++ src/main/java/com/corleone/avaliacao/controller/AvaliacaoController.java
@@ -9,7 +9,6 @@ import com.corleone.avaliacao.service.AvaliacaoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
