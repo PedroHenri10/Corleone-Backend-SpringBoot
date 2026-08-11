642b6735b651f10439afe52bc4f78e60b4cf8f49
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-11 09:02:04 -0300

    feat(cupom): add mapper

--- src/main/java/com/corleone/cupom/mapper/CupomMapper.java
+++ src/main/java/com/corleone/cupom/mapper/CupomMapper.java
@@ -1,4 +1,28 @@
 package com.corleone.cupom.mapper;
 
-public class CupomMapper {
+import com.corleone.cupom.dto.CupomRequest;
+import com.corleone.cupom.dto.CupomResponse;
+import com.corleone.cupom.dto.CupomResumoResponse;
+import com.corleone.cupom.entity.Cupom;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface CupomMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "ativo", constant = "true")
+    @Mapping(target = "dataCriacao", ignore = true)
+    Cupom toEntity(CupomRequest request);
+
+    CupomResponse toResponse(Cupom cupom);
+
+    CupomResumoResponse toResumoResponse(Cupom cupom);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "ativo", ignore = true)
+    @Mapping(target = "dataCriacao", ignore = true)
+    void updateEntity(@MappingTarget Cupom cupom, CupomRequest request);
 }
