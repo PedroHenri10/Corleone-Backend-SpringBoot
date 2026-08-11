8e95a006e1dcd8e8d80dc447f3f4093d65b13a72
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-11 16:29:15 -0300

    feat(cupom): validar cupom

--- src/main/java/com/corleone/cupom/validator/CupomValidator.java
+++ src/main/java/com/corleone/cupom/validator/CupomValidator.java
@@ -1,4 +1,24 @@
 package com.corleone.cupom.validator;
 
-public class CupomValidator{
+import com.corleone.cupom.entity.Cupom;
+import com.corleone.cupom.repository.CupomRepository;
+import com.corleone.exception.BusinessException;
+import com.corleone.exception.ResourceNotFoundException;
+import com.corleone.exceptionhandler.ErrorEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class CupomValidator {
+
+    private final CupomRepository repository;
+
+    public Cupom validarCupom(Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(ErrorEnum.CUPOM_NAO_ENCONTRADO));
+    }
+
+    
 }
