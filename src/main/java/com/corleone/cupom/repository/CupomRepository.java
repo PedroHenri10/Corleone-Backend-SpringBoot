44736678bf85d1b753ba1286ad551f0e7f9d7e22
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-11 08:58:40 -0300

    feat(cupom): add repository

--- src/main/java/com/corleone/cupom/repository/CupomRepository.java
+++ src/main/java/com/corleone/cupom/repository/CupomRepository.java
@@ -1,4 +1,15 @@
 package com.corleone.cupom.repository;
 
-public class CupomRepository {
+import com.corleone.cupom.entity.Cupom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.Optional;
+
+public interface CupomRepository extends JpaRepository<Cupom, Integer>, JpaSpecificationExecutor<Cupom> {
+
+    boolean existsByCodigo(String codigo);
+
+    Optional<Cupom> findByCodigo(String codigo);
+
 }
