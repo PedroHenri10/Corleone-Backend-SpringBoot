048ed7004d46816c29ed50adb3fce4f13dbe0cb8
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-12 14:17:47 -0300

    feat(cupom): specification codigo

--- src/main/java/com/corleone/cupom/specification/CupomSpecification.java
+++ src/main/java/com/corleone/cupom/specification/CupomSpecification.java
@@ -1,4 +1,15 @@
 package com.corleone.cupom.specification;
 
+import com.corleone.cupom.entity.Cupom;
+import org.springframework.data.jpa.domain.Specification;
+
 public class CupomSpecification {
+    public static Specification<Cupom> codigo(String codigo){
+        return (root, query,cb) ->
+                codigo == null || codigo.isBlank()
+                        ? null : cb.like(cb.lower(root.get("codigo")),
+                        "%" + codigo.toLowerCase() + "%");
+    }
+
+    
 }
