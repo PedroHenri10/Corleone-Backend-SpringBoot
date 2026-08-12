4e76e97f55328a82f144b97eb22e24ef6b0be99b
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-12 16:16:18 -0300

    feat(cupom): specification tipo cupom

--- src/main/java/com/corleone/cupom/specification/CupomSpecification.java
+++ src/main/java/com/corleone/cupom/specification/CupomSpecification.java
@@ -1,6 +1,7 @@
 package com.corleone.cupom.specification;
 
 import com.corleone.cupom.entity.Cupom;
+import com.corleone.shared.enums.TipoCupom;
 import org.springframework.data.jpa.domain.Specification;
 
 public class CupomSpecification {
@@ -9,6 +10,11 @@ public class CupomSpecification {
                 codigo == null || codigo.isBlank()
                         ? null : cb.like(cb.lower(root.get("codigo")),
                         "%" + codigo.toLowerCase() + "%");
+    }
+
+    public static Specification<Cupom> tipo(TipoCupom tipo){
+        return ((root, query, cb) ->
+                tipo == null ? null : cb.equal(root.get("tipo"), tipo));
     }
 
     
