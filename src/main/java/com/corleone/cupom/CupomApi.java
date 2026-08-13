b74e9a56be7d366d74e605f849c571e3cd11e582
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-13 11:57:57 -0300

    feat(cupom): documentar buscar por id

--- src/main/java/com/corleone/cupom/CupomApi.java
+++ src/main/java/com/corleone/cupom/CupomApi.java
@@ -3,6 +3,7 @@ package com.corleone.cupom;
 import com.corleone.cupom.dto.CupomRequest;
 import com.corleone.cupom.dto.CupomResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -23,5 +24,12 @@ public interface CupomApi {
             }
     )
     ResponseEntity<CupomResponse> criar(CupomRequest request);
-    
+
+    @Operation(summary = "Buscar cupom por ID", description = "Retorna os dados completos de um cupom.", responses = {
+                    @ApiResponse(responseCode = "200", description = "Cupom encontrado.", content = @Content(
+                            schema = @Schema(implementation = CupomResponse.class)
+                    )), @ApiResponse(responseCode = "404", description = "Cupom não encontrado.")
+            }
+    )
+    ResponseEntity<CupomResponse> buscarPorId(@Parameter(description = "ID do cupom.", example = "1") Integer id);
 }
