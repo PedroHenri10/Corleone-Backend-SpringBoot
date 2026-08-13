47956473950f0236d310d5d968d2d72446248244
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-13 11:21:28 -0300

    feat(cupom): criar cupom

--- src/main/java/com/corleone/cupom/service/CupomService.java
+++ src/main/java/com/corleone/cupom/service/CupomService.java
@@ -1,4 +1,41 @@
 package com.corleone.cupom.service;
 
+import com.corleone.cupom.dto.CupomRequest;
+import com.corleone.cupom.dto.CupomResponse;
+import com.corleone.cupom.entity.Cupom;
+import com.corleone.cupom.mapper.CupomMapper;
+import com.corleone.cupom.repository.CupomRepository;
+import com.corleone.cupom.validator.CupomValidator;
+import com.corleone.shared.util.DateUtils;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class CupomService {
-}
+    private final CupomRepository repository;
+    private final CupomMapper mapper;
+    private final CupomValidator validator;
+
+    public CupomResponse criar(CupomRequest request) {
+
+        validator.validarCodigoDuplicado(request.getCodigo());
+
+        validator.validarPeriodo(request.getDataInicio(), request.getDataFim());
+
+        validator.validarValorMinimo(request.getValorMinimo());
+
+        Cupom cupom = mapper.toEntity(request);
+
+        cupom.setDataCriacao(LocalDateTime.now(DateUtils.BR_ZONE));
+
+        cupom = repository.save(cupom);
+
+        return mapper.toResponse(cupom);
+    }
+
+    }
