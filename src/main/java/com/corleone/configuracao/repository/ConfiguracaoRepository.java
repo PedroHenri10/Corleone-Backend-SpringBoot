4b5a76bb18b05d528e96620196fe76a4c3ede453
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-14 10:54:52 -0300

    refactor(configuracao): repository id integer

--- src/main/java/com/corleone/configuracao/repository/ConfiguracaoRepository.java
+++ src/main/java/com/corleone/configuracao/repository/ConfiguracaoRepository.java
@@ -3,5 +3,5 @@ package com.corleone.configuracao.repository;
 import com.corleone.configuracao.entity.Configuracao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConfiguracaoRepository extends JpaRepository<Configuracao, Long> {
+public interface ConfiguracaoRepository extends JpaRepository<Configuracao, Integer> {
 }
