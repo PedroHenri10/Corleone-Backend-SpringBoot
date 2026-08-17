dcfc1ed65cb64569bc7269bca67c20fddc2d4b52
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-17 10:10:35 -0300

    feat(configuracao): validar configuracao

--- src/main/java/com/corleone/configuracao/validator/ConfiguracaoValidator.java
+++ src/main/java/com/corleone/configuracao/validator/ConfiguracaoValidator.java
@@ -1,4 +1,23 @@
 package com.corleone.configuracao.validator;
 
+import com.corleone.configuracao.entity.Configuracao;
+import com.corleone.configuracao.repository.ConfiguracaoRepository;
+import com.corleone.exception.BusinessException;
+import com.corleone.exception.ResourceNotFoundException;
+import com.corleone.exceptionhandler.ErrorEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class ConfiguracaoValidator {
+
+    private final ConfiguracaoRepository repository;
+
+    public Configuracao validarConfiguracao(Integer id) {
+        return repository.findById(id).orElseThrow(() ->
+                        new ResourceNotFoundException(ErrorEnum.CONFIGURACAO_NAO_ENCONTRADA));
+    }
+
+   
 }
