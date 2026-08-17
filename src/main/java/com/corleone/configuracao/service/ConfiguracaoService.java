13337812bcda441488347fb46848775df8b9e02b
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-17 11:33:16 -0300

    feat(configuracao): add criar

--- src/main/java/com/corleone/configuracao/service/ConfiguracaoService.java
+++ src/main/java/com/corleone/configuracao/service/ConfiguracaoService.java
@@ -1,4 +1,44 @@
 package com.corleone.configuracao.service;
 
+import com.corleone.configuracao.dto.ConfiguracaoRequest;
+import com.corleone.configuracao.dto.ConfiguracaoResponse;
+import com.corleone.configuracao.dto.ConfiguracaoResumoResponse;
+import com.corleone.configuracao.entity.Configuracao;
+import com.corleone.configuracao.mapper.ConfiguracaoMapper;
+import com.corleone.configuracao.repository.ConfiguracaoRepository;
+import com.corleone.configuracao.validator.ConfiguracaoValidator;
+import com.corleone.shared.util.DateUtils;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class ConfiguracaoService {
+
+    private final ConfiguracaoRepository repository;
+    private final ConfiguracaoMapper mapper;
+    private final ConfiguracaoValidator validator;
+
+    public ConfiguracaoResponse criar(ConfiguracaoRequest request) {
+
+        validator.validarChaveDuplicada(request.getChave());
+
+        Configuracao configuracao = mapper.toEntity(request);
+
+        LocalDateTime agora = LocalDateTime.now(DateUtils.BR_ZONE);
+
+        configuracao.setDataCriacao(agora);
+        configuracao.setDataAtualizacao(agora);
+
+        configuracao = repository.save(configuracao);
+
+        return mapper.toResponse(configuracao);
+    }
+
+    
 }
