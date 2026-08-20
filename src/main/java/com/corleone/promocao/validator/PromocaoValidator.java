d4d3f4154a0f67bfb8e9f430cb3795e45f7f0de3
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-20 09:57:34 -0300

    feat(promocao): injetando dependencias

--- src/main/java/com/corleone/promocao/validator/PromocaoValidator.java
+++ src/main/java/com/corleone/promocao/validator/PromocaoValidator.java
@@ -1,4 +1,16 @@
 package com.corleone.promocao.validator;
 
+import com.corleone.produto.repository.ProdutoRepository;
+import com.corleone.promocao.repository.PromocaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class PromocaoValidator {
+
+    private final PromocaoRepository repository;
+    private final ProdutoRepository produtoRepository;
+
+    
 }
