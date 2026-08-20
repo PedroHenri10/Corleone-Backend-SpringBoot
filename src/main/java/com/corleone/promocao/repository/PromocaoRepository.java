ecad9690f4870ed1a503d8d6d8819ba9246c06a7
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-20 09:46:51 -0300

    feat(promocao): add interface repository

--- src/main/java/com/corleone/promocao/repository/PromocaoRepository.java
+++ src/main/java/com/corleone/promocao/repository/PromocaoRepository.java
@@ -1,4 +1,10 @@
 package com.corleone.promocao.repository;
 
-public class PromocaoRepository {
+import com.corleone.promocao.entity.Promocao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface PromocaoRepository extends JpaRepository<Promocao, Integer>, JpaSpecificationExecutor<Promocao> {
+
+    boolean existsByProdutoId(Integer produtoId);
 }
