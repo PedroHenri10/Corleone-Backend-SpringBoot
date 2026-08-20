82e35656465ae78209cf6c28cab79a107ce1747c
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-20 09:51:37 -0300

    feat(promocao): add interface mapper

--- src/main/java/com/corleone/promocao/mapper/PromocaoMapper.java
+++ src/main/java/com/corleone/promocao/mapper/PromocaoMapper.java
@@ -1,4 +1,42 @@
 package com.corleone.promocao.mapper;
 
-public class PromocaoMapper {
+import com.corleone.promocao.dto.PromocaoRequest;
+import com.corleone.promocao.dto.PromocaoResponse;
+import com.corleone.promocao.dto.PromocaoResumoResponse;
+import com.corleone.promocao.entity.Promocao;
+import com.corleone.produto.entity.Produto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface PromocaoMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "produto", source = "produto")
+    @Mapping(target = "nome", source = "request.nome")
+    @Mapping(target = "descricao", source = "request.descricao")
+    @Mapping(target = "percentual", source = "request.percentual")
+    @Mapping(target = "dataInicio", source = "request.dataInicio")
+    @Mapping(target = "dataFim", source = "request.dataFim")
+    @Mapping(target = "ativa", source = "request.ativa")
+    Promocao toEntity(PromocaoRequest request, Produto produto);
+
+    @Mapping(target = "produtoId", source = "produto.id")
+    @Mapping(target = "produto", source = "produto.nome")
+    PromocaoResponse toResponse(Promocao promocao);
+
+    @Mapping(target = "produto", source = "produto.nome")
+    PromocaoResumoResponse toResumoResponse(Promocao promocao);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "produto", source = "produto")
+    @Mapping(target = "nome", source = "request.nome")
+    @Mapping(target = "descricao", source = "request.descricao")
+    @Mapping(target = "percentual", source = "request.percentual")
+    @Mapping(target = "dataInicio", source = "request.dataInicio")
+    @Mapping(target = "dataFim", source = "request.dataFim")
+    @Mapping(target = "ativa", source = "request.ativa")
+    void updateEntity(@MappingTarget Promocao promocao, PromocaoRequest request, Produto produto);
 }
