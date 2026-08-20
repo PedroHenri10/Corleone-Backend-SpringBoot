078e0de9f103b9bf62a4f9e11bcb32e756e85535
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-20 10:24:18 -0300

    feat(promocao): validar promocao

--- src/main/java/com/corleone/promocao/validator/PromocaoValidator.java
+++ src/main/java/com/corleone/promocao/validator/PromocaoValidator.java
@@ -1,6 +1,9 @@
 package com.corleone.promocao.validator;
 
+import com.corleone.exception.ResourceNotFoundException;
+import com.corleone.exceptionhandler.ErrorEnum;
 import com.corleone.produto.repository.ProdutoRepository;
+import com.corleone.promocao.entity.Promocao;
 import com.corleone.promocao.repository.PromocaoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,5 +15,9 @@ public class PromocaoValidator {
     private final PromocaoRepository repository;
     private final ProdutoRepository produtoRepository;
 
+    public Promocao validarPromocao(Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException(ErrorEnum.PROMOCAO_NAO_ENCONTRADA));
+    }
 
+    
 }
