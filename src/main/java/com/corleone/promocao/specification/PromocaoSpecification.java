b1cc35f08f159e4d637c54a774b66b534086d845
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-21 10:14:12 -0300

    feat(promocao): specification inicia

--- src/main/java/com/corleone/promocao/specification/PromocaoSpecification.java
+++ src/main/java/com/corleone/promocao/specification/PromocaoSpecification.java
@@ -1,4 +1,12 @@
 package com.corleone.promocao.specification;
 
+import com.corleone.promocao.dto.PromocaoFilter;
+import com.corleone.promocao.entity.Promocao;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDateTime;
+
 public class PromocaoSpecification {
+
+    
 }
--- src/main/java/com/corleone/promocao/validator/PromocaoValidator.java
+++ src/main/java/com/corleone/promocao/validator/PromocaoValidator.java
@@ -62,5 +62,5 @@ public class PromocaoValidator {
             throw new BusinessException(ErrorEnum.PROMOCAO_INATIVA);
         }
     }
-    
+
 }
