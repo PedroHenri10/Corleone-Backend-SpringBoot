d2cd983637fae8661fef1e2a229f28c2ceec54cb
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-25 16:09:44 -0300

    feat(promocao): implementar metodos da promocao api

--- src/main/java/com/corleone/promocao/controller/PromocaoController.java
+++ src/main/java/com/corleone/promocao/controller/PromocaoController.java
@@ -1,11 +1,63 @@
 package com.corleone.promocao.controller;
 
+import com.corleone.promocao.docs.PromocaoApi;
+import com.corleone.promocao.dto.PromocaoFilter;
+import com.corleone.promocao.dto.PromocaoRequest;
+import com.corleone.promocao.dto.PromocaoResponse;
+import com.corleone.promocao.dto.PromocaoResumoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/promocoes")
 @RequiredArgsConstructor
-public class PromocaoController {
+public class PromocaoController implements PromocaoApi {
+    @Override
+    public ResponseEntity<PromocaoResponse> criar(PromocaoRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<PromocaoResponse> buscarPorId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<PromocaoResumoResponse>> listar(PromocaoFilter filter) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<PromocaoResponse> atualizar(Integer id, PromocaoRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<PromocaoResponse> ativar(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<PromocaoResponse> desativar(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> excluir(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<PromocaoResumoResponse>> listarAtivas() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<PromocaoResumoResponse>> listarEmVigencia() {
+        return null;
+    }
 }
