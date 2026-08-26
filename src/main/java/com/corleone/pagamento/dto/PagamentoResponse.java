99a2a87a3c9f5ecd70a797790d0cdbce768763b1
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-26 08:41:52 -0300

    feat(pagamento): iniciar response

--- src/main/java/com/corleone/pagamento/dto/PagamentoResponse.java
+++ src/main/java/com/corleone/pagamento/dto/PagamentoResponse.java
@@ -1,4 +1,13 @@
 package com.corleone.pagamento.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Dados completos da forma de pagamento.")
 public class PagamentoResponse {
 }
