54b0f92fcea6c52ad4685f590d969c362eb11004
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-26 08:40:06 -0300

    feat(pagamento): add request

--- src/main/java/com/corleone/pagamento/dto/PagamentoRequest.java
+++ src/main/java/com/corleone/pagamento/dto/PagamentoRequest.java
@@ -1,6 +1,9 @@
 package com.corleone.pagamento.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -10,5 +13,13 @@ import lombok.*;
 @AllArgsConstructor
 @Schema(description = "DTO utilizado para cadastro e atualização de formas de pagamento.")
 public class PagamentoRequest {
-    
+
+    @NotBlank
+    @Size(max = 60)
+    @Schema(description = "Nome da forma de pagamento.", example = "Cartão de Crédito")
+    private String nome;
+
+    @NotNull
+    @Schema(description = "Indica se a forma de pagamento permite ou exige troco.", example = "false")
+    private Boolean requerTroco;
 }
