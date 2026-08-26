b474df1558977c0c86340ffa684e54c99203f82a
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-26 08:42:47 -0300

    feat(pagamento): add response

--- src/main/java/com/corleone/pagamento/dto/PagamentoResponse.java
+++ src/main/java/com/corleone/pagamento/dto/PagamentoResponse.java
@@ -10,4 +10,16 @@ import lombok.*;
 @AllArgsConstructor
 @Schema(description = "Dados completos da forma de pagamento.")
 public class PagamentoResponse {
+
+    @Schema(description = "ID da forma de pagamento.", example = "1")
+    private Integer id;
+
+    @Schema(description = "Nome da forma de pagamento.", example = "Cartão de Crédito")
+    private String nome;
+
+    @Schema(description = "Indica se a forma de pagamento requer troco.", example = "false")
+    private Boolean requerTroco;
+
+    @Schema(description = "Indica se a forma de pagamento está ativa.", example = "true")
+    private Boolean ativo;
 }
