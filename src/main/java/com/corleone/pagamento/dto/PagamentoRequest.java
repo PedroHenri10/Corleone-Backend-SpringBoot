b6469d69ca428bb50a6deaa8685f5e7d2e025901
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-26 08:38:48 -0300

    feat(pagamento): iniciar request

--- src/main/java/com/corleone/pagamento/dto/PagamentoRequest.java
+++ src/main/java/com/corleone/pagamento/dto/PagamentoRequest.java
@@ -1,4 +1,14 @@
 package com.corleone.pagamento.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO utilizado para cadastro e atualização de formas de pagamento.")
 public class PagamentoRequest {
+    
 }
