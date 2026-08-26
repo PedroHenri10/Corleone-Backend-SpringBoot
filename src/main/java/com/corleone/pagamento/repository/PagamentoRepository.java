41c6f5121edf818f771d6e2f0deb1e9c3ab0ac1b
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-26 08:46:20 -0300

    feat(pagamento): add interface repository

--- src/main/java/com/corleone/pagamento/repository/PagamentoRepository.java
+++ src/main/java/com/corleone/pagamento/repository/PagamentoRepository.java
@@ -1,4 +1,10 @@
 package com.corleone.pagamento.repository;
 
-public interface PagamentoRepository {
+import com.corleone.pagamento.entity.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>, JpaSpecificationExecutor<Pagamento> {
+
+    boolean existsByNomeIgnoreCase(String nome);
 }
