3d993b365178df8e71b8c2691190e98c4f1601a7
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-26 08:44:49 -0300

    feat(pagamento): add resumo response

--- src/main/java/com/corleone/pagamento/dto/PagamentoResumoResponse.java
+++ src/main/java/com/corleone/pagamento/dto/PagamentoResumoResponse.java
@@ -1,4 +1,22 @@
 package com.corleone.pagamento.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Resumo da forma de pagamento.")
 public class PagamentoResumoResponse {
+
+    @Schema(description = "ID da forma de pagamento.", example = "1")
+    private Integer id;
+
+    @Schema(description = "Nome da forma de pagamento.", example = "Pix")
+    private String nome;
+
+    @Schema(description = "Indica se a forma de pagamento está ativa.", example = "true")
+    private Boolean ativo;
 }
