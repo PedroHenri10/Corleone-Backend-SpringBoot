6099cd34e5b618f11589b963167b50aa7b41d2cf
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-27 11:28:03 -0300

    feat(pagamento): criar specification

---
+++ src/main/java/com/corleone/pagamento/specification/PagamentoSpecification.java
@@ -0,0 +1,9 @@
+package com.corleone.pagamento.specification;
+
+import com.corleone.pagamento.entity.Pagamento;
+import org.springframework.data.jpa.domain.Specification;
+
+public class PagamentoSpecification {
+
+    
+}
