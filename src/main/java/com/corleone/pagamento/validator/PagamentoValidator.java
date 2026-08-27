e35368ef5ada821b93af191809020e1d96682cde
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-27 09:13:33 -0300

    feat(pagamento): iniciar validator

--- src/main/java/com/corleone/pagamento/validator/PagamentoValidator.java
+++ src/main/java/com/corleone/pagamento/validator/PagamentoValidator.java
@@ -1,4 +1,10 @@
 package com.corleone.pagamento.validator;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class PagamentoValidator {
+    
 }
