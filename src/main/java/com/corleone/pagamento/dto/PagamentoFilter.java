a0d6ad201e62ee9c8f4fd3ae080b72e2dc9ab4e2
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-27 11:50:21 -0300

    feat(pagamento): add pagamento filter

--- src/main/java/com/corleone/pagamento/dto/PagamentoFilter.java
+++ src/main/java/com/corleone/pagamento/dto/PagamentoFilter.java
@@ -1,4 +1,19 @@
 package com.corleone.pagamento.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Filtros utilizados para consulta de formas de pagamento.")
 public class PagamentoFilter {
+
+    @Schema(description = "Nome ou parte do nome da forma de pagamento.", example = "Cartão")
+    private String nome;
+
+    @Schema(description = "Filtra pelo status da forma de pagamento.", example = "true")
+    private Boolean ativo;
 }
