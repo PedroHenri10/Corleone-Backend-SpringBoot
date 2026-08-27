06b5561e082b85298434401d2c712d48fa068f73
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-27 09:15:46 -0300

    feat(pagamento): instanciar repository

--- src/main/java/com/corleone/pagamento/validator/PagamentoValidator.java
+++ src/main/java/com/corleone/pagamento/validator/PagamentoValidator.java
@@ -1,10 +1,12 @@
 package com.corleone.pagamento.validator;
 
+import com.corleone.pagamento.repository.PagamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class PagamentoValidator {
-    
+    private final PagamentoRepository repository;
+
 }
