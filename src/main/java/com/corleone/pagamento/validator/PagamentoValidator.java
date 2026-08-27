a699ec5b56bfb3da491e87170116c532a8bc7f44
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-27 09:27:55 -0300

    feat(pagamento): validar pagamento

--- src/main/java/com/corleone/pagamento/validator/PagamentoValidator.java
+++ src/main/java/com/corleone/pagamento/validator/PagamentoValidator.java
@@ -1,5 +1,8 @@
 package com.corleone.pagamento.validator;
 
+import com.corleone.exception.ResourceNotFoundException;
+import com.corleone.exceptionhandler.ErrorEnum;
+import com.corleone.pagamento.entity.Pagamento;
 import com.corleone.pagamento.repository.PagamentoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -9,4 +12,8 @@ import org.springframework.stereotype.Component;
 public class PagamentoValidator {
     private final PagamentoRepository repository;
 
+    public Pagamento validarPagamento(Integer id) {
+        return repository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException(ErrorEnum.PAGAMENTO_NAO_ENCONTRADO));
+    }
 }
