5f921d0dc6147cf8651072eba4a79878960f92db
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-27 11:40:17 -0300

    feat(pagamento): specification

--- src/main/java/com/corleone/pagamento/specification/PagamentoSpecification.java
+++ src/main/java/com/corleone/pagamento/specification/PagamentoSpecification.java
@@ -5,5 +5,12 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class PagamentoSpecification {
 
-    
+    public static Specification<Pagamento> nome(String nome) {
+        return (root, query, cb) ->
+                nome == null || nome.isBlank() ? null : cb.like(cb.lower(root.get("nome")),
+                        "%" + nome.toLowerCase() + "%"
+                );
+    }
+
+
 }
