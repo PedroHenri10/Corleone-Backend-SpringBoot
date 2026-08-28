b065ec4632e4c7515b5c78192148030ab2907c0d
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-28 08:51:26 -0300

    feat(pagamento): iniciar service

--- src/main/java/com/corleone/pagamento/service/PagamentoService.java
+++ src/main/java/com/corleone/pagamento/service/PagamentoService.java
@@ -1,4 +1,11 @@
 package com.corleone.pagamento.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class PagamentoService {
 }
