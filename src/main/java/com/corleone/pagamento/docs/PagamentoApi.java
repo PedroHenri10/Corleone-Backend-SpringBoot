ab7b9c3bb0c3be07d13d2600cbdd82d02f442f86
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-31 09:48:53 -0300

    fix(pagamento): exluir @ do interface

--- src/main/java/com/corleone/pagamento/docs/PagamentoApi.java
+++ src/main/java/com/corleone/pagamento/docs/PagamentoApi.java
@@ -1,9 +1,16 @@
 package com.corleone.pagamento.docs;
 
+import com.corleone.pagamento.dto.PagamentoRequest;
+import com.corleone.pagamento.dto.PagamentoResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 
 @Tag(name = "Pagamento", description = "Gerenciamento das formas de pagamento do sistema.")
-public @interface PagamentoApi {
+public interface PagamentoApi {
 
     @Operation(summary = "Cadastrar forma de pagamento", description = "Realiza o cadastro de uma nova forma de pagamento.",
             responses = {
