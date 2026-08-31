bb3186ef7bfcce407d5572fe59c8fd45e8647e6d
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-31 09:37:24 -0300

    feat(pagamento): iniciar documentacao api

--- src/main/java/com/corleone/pagamento/docs/PagamentoApi.java
+++ src/main/java/com/corleone/pagamento/docs/PagamentoApi.java
@@ -2,5 +2,6 @@ package com.corleone.pagamento.docs;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "Pagamento", description = "Gerenciamento das formas de pagamento do sistema.")
 public @interface PagamentoApi {
 }
