49c8c6679d3fe09551b621554681d7d4fa5edb66
Pedro Henrique <dinhonoliver@gmail.com>
2026-08-31 09:40:36 -0300

    feat(pagamento): documentar criat

--- src/main/java/com/corleone/pagamento/docs/PagamentoApi.java
+++ src/main/java/com/corleone/pagamento/docs/PagamentoApi.java
@@ -4,4 +4,19 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Pagamento", description = "Gerenciamento das formas de pagamento do sistema.")
 public @interface PagamentoApi {
+
+    @Operation(summary = "Cadastrar forma de pagamento", description = "Realiza o cadastro de uma nova forma de pagamento.",
+            responses = {
+                @ApiResponse(responseCode = "200", description = "Forma de pagamento cadastrada com sucesso.",
+                            content = @Content(
+                                    schema = @Schema(
+                                            implementation = PagamentoResponse.class
+                                    )
+                            )
+                    ),
+                    @ApiResponse(responseCode = "409", description = "Forma de pagamento já cadastrada."),
+                    @ApiResponse(responseCode = "422", description = "Dados da forma de pagamento inválidos.")
+            }
+    )
+    ResponseEntity<PagamentoResponse> criar(PagamentoRequest request);
 }
