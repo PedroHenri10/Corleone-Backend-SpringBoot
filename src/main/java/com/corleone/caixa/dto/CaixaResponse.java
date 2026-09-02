318ac8a437f781ffc8f9067e27d2c95fc7c120cd
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-02 09:57:09 -0300

    feat(caixa): iniciar response

--- src/main/java/com/corleone/caixa/dto/CaixaResponse.java
+++ src/main/java/com/corleone/caixa/dto/CaixaResponse.java
@@ -1,4 +1,14 @@
 package com.corleone.caixa.dto;
 
+import com.corleone.shared.enums.StatusCaixa;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO de retorno completo do caixa.")
 public class CaixaResponse {
 }
