ceb1fd38b210fc11760205c2ccb0ac3691a506f7
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-02 11:24:02 -0300

    feat(caixa): add filter

--- src/main/java/com/corleone/caixa/dto/CaixaFilter.java
+++ src/main/java/com/corleone/caixa/dto/CaixaFilter.java
@@ -1,7 +1,10 @@
 package com.corleone.caixa.dto;
 
+import com.corleone.shared.enums.StatusCaixa;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -10,4 +13,16 @@ import lombok.*;
 @AllArgsConstructor
 @Schema(description = "Filtros utilizados para consulta de caixas.")
 public class CaixaFilter {
+
+    @Schema(description = "ID do funcionário responsável.", example = "10")
+    private Integer funcionarioId;
+
+    @Schema(description = "Status do caixa.", example = "ABERTO")
+    private StatusCaixa status;
+
+    @Schema(description = "Data inicial da pesquisa.", example = "2026-09-01")
+    private LocalDate dataInicial;
+
+    @Schema(description = "Data final da pesquisa.", example = "2026-09-30")
+    private LocalDate dataFinal;
 }
