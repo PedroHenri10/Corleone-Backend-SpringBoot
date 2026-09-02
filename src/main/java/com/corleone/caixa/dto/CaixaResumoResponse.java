2549eb3545d5fbfd034e480abee41718e73d2d72
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-02 10:06:41 -0300

    feat(caixa): iniciar resumo response

--- src/main/java/com/corleone/caixa/dto/CaixaResumoResponse.java
+++ src/main/java/com/corleone/caixa/dto/CaixaResumoResponse.java
@@ -1,4 +1,13 @@
 package com.corleone.caixa.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO resumido utilizado na listagem de caixas.")
 public class CaixaResumoResponse {
 }
