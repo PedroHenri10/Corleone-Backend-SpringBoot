9e4e920f2d2bc86ded053101cf426a7cb5129301
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-02 09:53:49 -0300

    feat(caixa): iniciar request

--- src/main/java/com/corleone/caixa/dto/CaixaRequest.java
+++ src/main/java/com/corleone/caixa/dto/CaixaRequest.java
@@ -1,4 +1,13 @@
 package com.corleone.caixa.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO utilizado para abertura e atualização dos dados do caixa.")
 public class CaixaRequest {
 }
