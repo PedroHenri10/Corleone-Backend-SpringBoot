3a4842f78ac0d862c6c1a07e90deec1857a2e8d6
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-02 11:23:34 -0300

    feat(caixa): iniciar filter

--- src/main/java/com/corleone/caixa/dto/CaixaFilter.java
+++ src/main/java/com/corleone/caixa/dto/CaixaFilter.java
@@ -1,4 +1,13 @@
 package com.corleone.caixa.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Filtros utilizados para consulta de caixas.")
 public class CaixaFilter {
 }
