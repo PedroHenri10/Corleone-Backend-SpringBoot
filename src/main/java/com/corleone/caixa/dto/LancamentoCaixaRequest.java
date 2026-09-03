5bdc1d971df3f4c87036abfa284056cf28d5b22e
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-03 09:17:59 -0300

    feat(caixa): iniciar lancamento caixa request dto

--- src/main/java/com/corleone/caixa/dto/LancamentoCaixaRequest.java
+++ src/main/java/com/corleone/caixa/dto/LancamentoCaixaRequest.java
@@ -1,5 +1,16 @@
 package com.corleone.caixa.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO de retorno completo de um lançamento de caixa.")
 public class LancamentoCaixaRequest {
 }
