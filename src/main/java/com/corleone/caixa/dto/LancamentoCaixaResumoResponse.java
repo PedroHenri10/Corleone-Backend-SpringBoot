55ff586b0ed229c4615c5faf3ef081936a55f8ac
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-03 09:19:22 -0300

    feat(caixa): iniciar lancamento caixa resumo response dto

--- src/main/java/com/corleone/caixa/dto/LancamentoCaixaResumoResponse.java
+++ src/main/java/com/corleone/caixa/dto/LancamentoCaixaResumoResponse.java
@@ -1,4 +1,14 @@
 package com.corleone.caixa.dto;
 
+import com.corleone.shared.enums.TipoLancamentoCaixa;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO resumido utilizado na listagem de lançamentos de caixa.")
 public class LancamentoCaixaResumoResponse {
 }
