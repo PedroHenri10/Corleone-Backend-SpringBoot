2e17f23388fb996915cce511671a07247edda74e
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-03 09:20:14 -0300

    feat(caixa): iniciar lancamento caixa filter dto

--- src/main/java/com/corleone/caixa/dto/LancamentoCaixaFilter.java
+++ src/main/java/com/corleone/caixa/dto/LancamentoCaixaFilter.java
@@ -1,4 +1,16 @@
 package com.corleone.caixa.dto;
 
+import com.corleone.shared.enums.TipoLancamentoCaixa;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Filtros utilizados para consulta de lançamentos de caixa.")
 public class LancamentoCaixaFilter {
 }
