e3bb891b9623846f02dc6065794cf253995202f0
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-03 09:14:52 -0300

    feat(caixa): iniciar lancamento caixa response dto

--- src/main/java/com/corleone/caixa/dto/LancamentoCaixaResponse.java
+++ src/main/java/com/corleone/caixa/dto/LancamentoCaixaResponse.java
@@ -1,4 +1,14 @@
 package com.corleone.caixa.dto;
 
+import com.corleone.shared.enums.TipoLancamentoCaixa;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO de retorno completo de um lançamento de caixa.")
 public class LancamentoCaixaResponse {
 }
