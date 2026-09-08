06ebe2ea5d06fb7421818c37a0cda0af3877229a
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-08 09:13:52 -0300

    feat(caixa): instanciar no validator

--- src/main/java/com/corleone/caixa/validator/CaixaValidator.java
+++ src/main/java/com/corleone/caixa/validator/CaixaValidator.java
@@ -1,9 +1,17 @@
 package com.corleone.caixa.validator;
 
+import com.corleone.caixa.repository.CaixaRepository;
+import com.corleone.caixa.repository.LancamentoCaixaRepository;
+import com.corleone.funcionario.repository.FuncionarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class CaixaValidator {
+    private final CaixaRepository repository;
+    private final FuncionarioRepository funcionarioRepository;
+    private final LancamentoCaixaRepository lancamentoRepository;
+
+    
 }
