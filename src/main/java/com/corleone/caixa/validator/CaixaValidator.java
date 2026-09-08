7ca69ab92c0b4056f37ff63dd4a8b3612269e9bc
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-08 09:24:29 -0300

    feat(caixa): validar funcionario sem caixa aberto

--- src/main/java/com/corleone/caixa/validator/CaixaValidator.java
+++ src/main/java/com/corleone/caixa/validator/CaixaValidator.java
@@ -3,10 +3,12 @@ package com.corleone.caixa.validator;
 import com.corleone.caixa.entity.Caixa;
 import com.corleone.caixa.repository.CaixaRepository;
 import com.corleone.caixa.repository.LancamentoCaixaRepository;
+import com.corleone.exception.BusinessException;
 import com.corleone.exception.ResourceNotFoundException;
 import com.corleone.exceptionhandler.ErrorEnum;
 import com.corleone.funcionario.entity.Funcionario;
 import com.corleone.funcionario.repository.FuncionarioRepository;
+import com.corleone.shared.enums.StatusCaixa;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -24,5 +26,11 @@ public class CaixaValidator {
     public Funcionario validarFuncionario(Integer id) {
         return funcionarioRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(
                                 ErrorEnum.FUNCIONARIO_NAO_ENCONTRADO));
+    }
+
+    public void validarFuncionarioSemCaixaAberto(Integer funcionarioId) {
+        if (repository.existsByFuncionarioIdAndStatus(funcionarioId, StatusCaixa.ABERTO)) {
+            throw new BusinessException(ErrorEnum.CAIXA_JA_ABERTO);
+        }
     }
 }
