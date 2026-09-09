ca523d77e5a0790c802883e22eebda036e44e907
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-09 10:45:33 -0300

    feat(caixa): criar specification

---
+++ src/main/java/com/corleone/caixa/specification/CaixaSpecification.java
@@ -0,0 +1,10 @@
+package com.corleone.caixa.specification;
+
+import com.corleone.caixa.entity.Caixa;
+import org.springframework.data.jpa.domain.Specification;
+
+public class CaixaSpecification {
+
+    
+
+}
