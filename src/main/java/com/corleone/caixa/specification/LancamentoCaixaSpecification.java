5cef38d57bf639367ccf47206bbc08992aa83f2e
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-09 11:48:12 -0300

    feat(caixa): caixa id

--- src/main/java/com/corleone/caixa/specification/LancamentoCaixaSpecification.java
+++ src/main/java/com/corleone/caixa/specification/LancamentoCaixaSpecification.java
@@ -1,4 +1,18 @@
 package com.corleone.caixa.specification;
 
+import com.corleone.caixa.entity.LancamentoCaixa;
+import org.springframework.data.jpa.domain.Specification;
+
 public class LancamentoCaixaSpecification {
+    public static Specification<LancamentoCaixa> caixaId(Integer caixaId) {
+
+        return (root, query, cb) ->
+                caixaId == null
+                        ? null
+                        : cb.equal(
+                        root.get("caixa").get("id"),
+                        caixaId
+                );
+    }
+
 }
