5de8a72bf4fe74249dca3987c1cf66543e774a8c
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-11 11:45:23 -0300

    feat(caixa): add abrir caixa

--- src/main/java/com/corleone/caixa/service/CaixaService.java
+++ src/main/java/com/corleone/caixa/service/CaixaService.java
@@ -1,11 +1,18 @@
 package com.corleone.caixa.service;
 
+import com.corleone.caixa.dto.CaixaRequest;
+import com.corleone.caixa.dto.CaixaResponse;
+import com.corleone.caixa.entity.Caixa;
 import com.corleone.caixa.mapper.CaixaMapper;
 import com.corleone.caixa.repository.CaixaRepository;
 import com.corleone.caixa.validator.CaixaValidator;
+import com.corleone.shared.enums.StatusCaixa;
+import com.corleone.shared.util.DateUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +22,27 @@ public class CaixaService {
     private final CaixaRepository repository;
     private final CaixaMapper mapper;
     private final CaixaValidator validator;
+
+    public CaixaResponse abrir(CaixaRequest request) {
+
+        validator.validarFuncionario(request.getFuncionarioId());
+
+        validator.validarFuncionarioSemCaixaAberto(request.getFuncionarioId());
+
+        validator.validarValorAbertura(request.getValorAbertura());
+
+        var funcionario = validator.validarFuncionario(request.getFuncionarioId());
+
+        Caixa caixa = mapper.toEntity(request, funcionario);
+
+        caixa.setDataAbertura(LocalDateTime.now(DateUtils.BR_ZONE));
+
+        caixa.setStatus(StatusCaixa.ABERTO);
+
+        caixa = repository.save(caixa);
+
+        return mapper.toResponse(caixa);
+    }
 
     
 }
