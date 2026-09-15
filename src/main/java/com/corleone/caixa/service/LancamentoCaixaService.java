f3801fafefa95ee71f249a37353ecb79d44f587c
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-15 10:43:53 -0300

    (Caixa): iniciar lancamento caixa service

--- src/main/java/com/corleone/caixa/service/LancamentoCaixaService.java
+++ src/main/java/com/corleone/caixa/service/LancamentoCaixaService.java
@@ -1,5 +1,12 @@
 package com.corleone.caixa.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class LancamentoCaixaService {
+    
 }
