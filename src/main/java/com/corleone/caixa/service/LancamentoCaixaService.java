248598934f1315e00ce6d6c9c8ab010f9bd03f83
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-15 10:44:51 -0300

    (Caixa): instanciar no lancamento caixa service

--- src/main/java/com/corleone/caixa/service/LancamentoCaixaService.java
+++ src/main/java/com/corleone/caixa/service/LancamentoCaixaService.java
@@ -1,5 +1,8 @@
 package com.corleone.caixa.service;
 
+import com.corleone.caixa.mapper.CaixaMapper;
+import com.corleone.caixa.repository.LancamentoCaixaRepository;
+import com.corleone.caixa.validator.CaixaValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,5 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class LancamentoCaixaService {
-    
+    private final LancamentoCaixaRepository repository;
+    private final CaixaValidator validator;
+    private final CaixaMapper mapper;
+
 }
