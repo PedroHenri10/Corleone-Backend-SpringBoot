3963e704474d3d688554cb533a26acb503ebdd5d
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-15 10:47:02 -0300

    (Caixa): instanciar funcionario repository no lancamento caixa service

--- src/main/java/com/corleone/caixa/service/LancamentoCaixaService.java
+++ src/main/java/com/corleone/caixa/service/LancamentoCaixaService.java
@@ -3,6 +3,7 @@ package com.corleone.caixa.service;
 import com.corleone.caixa.mapper.CaixaMapper;
 import com.corleone.caixa.repository.LancamentoCaixaRepository;
 import com.corleone.caixa.validator.CaixaValidator;
+import com.corleone.funcionario.repository.FuncionarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +15,7 @@ public class LancamentoCaixaService {
     private final LancamentoCaixaRepository repository;
     private final CaixaValidator validator;
     private final CaixaMapper mapper;
+
+    private final FuncionarioRepository funcionarioRepository;
 
 }
