cb874836d887045dba2b848eec8b9af56b949f82
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-16 09:55:43 -0300

    (Caixa): add request mapping

--- src/main/java/com/corleone/caixa/docs/CaixaApi.java
+++ src/main/java/com/corleone/caixa/docs/CaixaApi.java
@@ -1,10 +1,9 @@
 package com.corleone.caixa.docs;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Tag(
-        name = "Caixa",
-        description = "Operações relacionadas ao controle e movimentação dos caixas da pizzaria."
-)
+@Tag(name = "Caixa", description = "Operações relacionadas ao controle e movimentação dos caixas da pizzaria.")
+@RequestMapping("/v1/api/caixas")
 public interface CaixaApi {
 }
