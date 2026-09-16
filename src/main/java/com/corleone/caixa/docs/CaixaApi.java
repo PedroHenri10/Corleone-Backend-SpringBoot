e3c77f8748fdc31fb05f98e669bdf12c3ec83673
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-16 09:52:56 -0300

    (Caixa): add tag documentao

--- src/main/java/com/corleone/caixa/docs/CaixaApi.java
+++ src/main/java/com/corleone/caixa/docs/CaixaApi.java
@@ -1,4 +1,10 @@
 package com.corleone.caixa.docs;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(
+        name = "Caixa",
+        description = "Operações relacionadas ao controle e movimentação dos caixas da pizzaria."
+)
 public interface CaixaApi {
 }
