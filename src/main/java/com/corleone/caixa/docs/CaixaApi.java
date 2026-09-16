99824efc869b13836484298244bc99b15cdd5c36
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-16 09:58:13 -0300

    (Caixa): add operacao abrir

--- src/main/java/com/corleone/caixa/docs/CaixaApi.java
+++ src/main/java/com/corleone/caixa/docs/CaixaApi.java
@@ -1,9 +1,22 @@
 package com.corleone.caixa.docs;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "Caixa", description = "Operações relacionadas ao controle e movimentação dos caixas da pizzaria.")
 @RequestMapping("/v1/api/caixas")
 public interface CaixaApi {
+
+    @Operation(summary = "Abrir caixa", description = """
+                    Abre um novo caixa para um funcionário.
+
+                    O funcionário não pode possuir outro caixa aberto.
+                    O valor de abertura deve ser maior ou igual a zero.
+                    """
+    )
+    @PostMapping
+    ResponseEntity<CaixaResponse> abrir(
+            @Valid @RequestBody CaixaRequest request
+    );
 }
