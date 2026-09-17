fca85d8e0f5a36c47c22de299c263935b87ea973
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-17 09:09:57 -0300

    refactor(Caixa): implementando metodos

--- src/main/java/com/corleone/caixa/controller/CaixaController.java
+++ src/main/java/com/corleone/caixa/controller/CaixaController.java
@@ -1,6 +1,39 @@
 package com.corleone.caixa.controller;
 
 import com.corleone.caixa.docs.CaixaApi;
+import com.corleone.caixa.dto.CaixaRequest;
+import com.corleone.caixa.dto.CaixaResponse;
+import com.corleone.caixa.dto.CaixaResumoResponse;
+import com.corleone.shared.enums.StatusCaixa;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 public class CaixaController implements CaixaApi {
+    @Override
+    public ResponseEntity<CaixaResponse> abrir(CaixaRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<CaixaResponse> fechar(Integer id, BigDecimal valorFechamento) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<CaixaResponse> buscarPorId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<CaixaResumoResponse>> listar(Integer funcionarioId, StatusCaixa status, LocalDate dataInicial, LocalDate dataFinal) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<CaixaResumoResponse>> listarAbertos() {
+        return null;
+    }
 }
