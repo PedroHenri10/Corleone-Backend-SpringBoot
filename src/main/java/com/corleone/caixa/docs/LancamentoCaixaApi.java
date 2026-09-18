98f8a37cc81510e9c99a6de31b1b274f18c0dda6
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-18 10:18:23 -0300

    feat(caixa): request mapping

--- src/main/java/com/corleone/caixa/docs/LancamentoCaixaApi.java
+++ src/main/java/com/corleone/caixa/docs/LancamentoCaixaApi.java
@@ -1,8 +1,10 @@
 package com.corleone.caixa.docs;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "Lançamentos de Caixa", description = "Operações relacionadas aos lançamentos realizados nos caixas.")
+@RequestMapping("/v1/api/lancamentos-caixa")
 public interface LancamentoCaixaApi {
     
 }
