b078863e5cdced4c1076d3e3f8dfeac3a9a58edc
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-18 11:01:59 -0300

    feat(caixa): documentar buscar por id

--- src/main/java/com/corleone/caixa/docs/LancamentoCaixaApi.java
+++ src/main/java/com/corleone/caixa/docs/LancamentoCaixaApi.java
@@ -3,6 +3,8 @@ package com.corleone.caixa.docs;
 import com.corleone.caixa.dto.LancamentoCaixaRequest;
 import com.corleone.caixa.dto.LancamentoCaixaResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -10,9 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Lançamentos de Caixa", description = "Operações relacionadas aos lançamentos realizados nos caixas.")
 @RequestMapping("/v1/api/lancamentos-caixa")
@@ -55,5 +55,30 @@ public interface LancamentoCaixaApi {
     @PostMapping
     ResponseEntity<LancamentoCaixaResponse> criar(@Valid @RequestBody LancamentoCaixaRequest request);
 
-    
+    @Operation(summary = "Buscar lançamento por ID", description = "Consulta um lançamento de caixa pelo seu identificador.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Lançamento encontrado.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(
+                                    implementation = LancamentoCaixaResponse.class
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Lançamento não encontrado.",
+                    content = @Content
+            )
+    })
+    @GetMapping("/{id}")
+    ResponseEntity<LancamentoCaixaResponse> buscarPorId(@Parameter(
+                    description = "ID do lançamento.",
+                    example = "1",
+                    required = true,
+                    in = ParameterIn.PATH
+    ) @PathVariable Integer id);
+
 }
