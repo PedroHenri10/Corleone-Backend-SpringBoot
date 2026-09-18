60982e0f0d85c43226477d69e2ef1b26a8003daa
Pedro Henrique <dinhonoliver@gmail.com>
2026-09-18 10:17:20 -0300

    feat(caixa): tag da api lancamento

--- src/main/java/com/corleone/caixa/docs/LancamentoCaixaApi.java
+++ src/main/java/com/corleone/caixa/docs/LancamentoCaixaApi.java
@@ -1,4 +1,8 @@
 package com.corleone.caixa.docs;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Lançamentos de Caixa", description = "Operações relacionadas aos lançamentos realizados nos caixas.")
 public interface LancamentoCaixaApi {
+    
 }
