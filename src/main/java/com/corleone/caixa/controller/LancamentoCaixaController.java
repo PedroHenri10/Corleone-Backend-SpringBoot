d0d98ffc3935a932ff3ccdd7a3b9d6267ef6189c
Pedro Henrique Nunes Oliveira <pedro.nunes@foursys.com.br>
2026-09-22 11:28:50 -0300

    feat(caixa): implementar lancamento caixa api

--- src/main/java/com/corleone/caixa/controller/LancamentoCaixaController.java
+++ src/main/java/com/corleone/caixa/controller/LancamentoCaixaController.java
@@ -1,9 +1,37 @@
 package com.corleone.caixa.controller;
 
+import com.corleone.caixa.docs.LancamentoCaixaApi;
+import com.corleone.caixa.dto.LancamentoCaixaRequest;
+import com.corleone.caixa.dto.LancamentoCaixaResponse;
+import com.corleone.caixa.dto.LancamentoCaixaResumoResponse;
+import com.corleone.shared.enums.TipoLancamentoCaixa;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class LancamentoCaixaController {
+public class LancamentoCaixaController implements LancamentoCaixaApi {
+    @Override
+    public ResponseEntity<LancamentoCaixaResponse> criar(LancamentoCaixaRequest request) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<LancamentoCaixaResponse> buscarPorId(Integer id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<LancamentoCaixaResumoResponse>> listar(Integer caixaId, Integer funcionarioId, Integer pedidoId, Integer pagamentoId, TipoLancamentoCaixa tipo, LocalDate dataInicial, LocalDate dataFinal) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<List<LancamentoCaixaResumoResponse>> listarPorCaixa(Integer caixaId) {
+        return null;
+    }
 }
