a050dc87daf0f7dba8edd5007da7fdfc69fb31f4
Pedro Henrique Nunes Oliveira <dinhonoliver@gmail.com>
2026-09-23 10:37:20 -0300

    feat(pedido): add historico pedido repository

--- src/main/java/com/corleone/pedido/repository/HistoricoPedidoRepository.java
+++ src/main/java/com/corleone/pedido/repository/HistoricoPedidoRepository.java
@@ -1,4 +1,12 @@
 package com.corleone.pedido.repository;
 
-public interface HistoricoPedidoRepository {
+import com.corleone.pedido.entity.HistoricoPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+public interface HistoricoPedidoRepository extends JpaRepository<HistoricoPedido, Integer>, JpaSpecificationExecutor<HistoricoPedido> {
+
+    List<HistoricoPedido> findByPedidoId(Integer pedidoId);
 }
