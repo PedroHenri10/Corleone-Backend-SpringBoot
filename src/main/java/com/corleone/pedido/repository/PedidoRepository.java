6ed6f51066a3c56b856c8318f02d5ed0802e5e42
Pedro Henrique Nunes Oliveira <pedro.nunes@foursys.com.br>
2026-09-23 10:29:31 -0300

    feat(pedido): add pedido repository

--- src/main/java/com/corleone/pedido/repository/PedidoRepository.java
+++ src/main/java/com/corleone/pedido/repository/PedidoRepository.java
@@ -2,6 +2,7 @@ package com.corleone.pedido.repository;
 
 import com.corleone.pedido.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>, JpaSpecificationExecutor<Pedido> {
 }
