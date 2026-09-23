aa16f8ffe45d45ac0d68a278e44dcb796617dcee
Pedro Henrique Nunes Oliveira <dinhonoliver@gmail.com>
2026-09-23 10:34:37 -0300

    feat(pedido): add item pedido repository

--- src/main/java/com/corleone/pedido/repository/ItemPedidoRepository.java
+++ src/main/java/com/corleone/pedido/repository/ItemPedidoRepository.java
@@ -1,4 +1,12 @@
 package com.corleone.pedido.repository;
 
-public interface ItemPedidoRepository {
+import com.corleone.pedido.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.List;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>, JpaSpecificationExecutor<ItemPedido> {
+
+    List<ItemPedido> findByPedidoId(Integer pedidoId);
 }
