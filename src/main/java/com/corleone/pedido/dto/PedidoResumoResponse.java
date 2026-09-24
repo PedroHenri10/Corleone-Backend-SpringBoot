f6f88f72bc7b149863c30b0bf811f7c0beed3dad
Pedro Henrique Nunes Oliveira <dinhonoliver@gmail.com>
2026-09-24 10:25:04 -0300

    feat(pedido): add pedido resumo response

--- src/main/java/com/corleone/pedido/dto/PedidoResumoResponse.java
+++ src/main/java/com/corleone/pedido/dto/PedidoResumoResponse.java
@@ -1,4 +1,35 @@
 package com.corleone.pedido.dto;
 
+import com.corleone.shared.enums.StatusPedido;
+import com.corleone.shared.enums.TipoPedido;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO resumido do pedido.")
 public class PedidoResumoResponse {
+
+    @Schema(example = "1")
+    private Integer id;
+
+    @Schema(example = "João da Silva")
+    private String cliente;
+
+    @Schema(example = "Carlos")
+    private String funcionario;
+
+    private TipoPedido tipo;
+
+    private BigDecimal total;
+
+    private StatusPedido status;
+
+    private LocalDateTime dataCriacao;
 }
