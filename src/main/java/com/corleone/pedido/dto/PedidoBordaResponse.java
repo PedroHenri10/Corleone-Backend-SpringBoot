e373922afd7e0fc5aae03d5450e93944b4524274
Pedro Henrique Nunes Oliveira <dinhonoliver@gmail.com>
2026-09-25 09:40:36 -0300

    feat(pedido): add pedido borda response

--- src/main/java/com/corleone/pedido/dto/PedidoBordaResponse.java
+++ src/main/java/com/corleone/pedido/dto/PedidoBordaResponse.java
@@ -1,4 +1,25 @@
 package com.corleone.pedido.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "DTO de resposta da borda associada ao item do pedido.")
 public class PedidoBordaResponse {
+
+    @Schema(example = "1")
+    private Integer id;
+
+    @Schema(example = "10")
+    private Integer itemPedidoId;
+
+    @Schema(example = "2")
+    private Integer bordaId;
+
+    @Schema(example = "Catupiry")
+    private String borda;
 }
